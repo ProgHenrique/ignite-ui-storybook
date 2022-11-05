b20d2a5e7d83f7a8cdc03fbbde0fcf996f7e1de5
ProgHenrique <henrique123187@gmail.com>
2022-11-05 09:55:39 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/ProgHenrique.png',
+    alt: 'Henrique Ramos',
+  },
+
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+export const withFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,44 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    size: 'md',
+    children:
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsa voluptas iste porro animi officia error? Suscipit accusantium quibusdam iure provident quae, voluptatum voluptate odio harum alias similique veritatis autem.',
+  },
+
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
